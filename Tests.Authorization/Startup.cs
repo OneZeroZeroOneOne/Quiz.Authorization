834a81f8b28f1d681e756e80ae09ceb37dd8fe40
OneZeroZeroOneOne <proxorenko200055@gmail.com>
2020-12-08 20:42:06 +0200

    deploy

--- Tests.Authorization/Startup.cs
+++ Tests.Authorization/Startup.cs
@@ -29,6 +29,7 @@ namespace Tests.Authorization
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
 
             services.AddSingleton(new AuthOptions());
